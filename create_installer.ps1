8b3749ef3b1ee6c73e0ce0a0eebb746fcd0bebc2
bflach <flach.bence@gmail.com>
2022-10-23 00:24:38 +0200

    Update create_installer.ps1 with virtual environment activation

--- create_installer.ps1
+++ create_installer.ps1
@@ -2,5 +2,8 @@
 $git_sha = git rev-parse --short HEAD
 $Filename = "mdf-channel-rename_" +  $git_sha
 
+# Activate virtual environment 
+.\venv\Scripts\activate
+
 # Configure pyinstaller - create single file, use icon
 pyinstaller -F -i gui_rename_icon_157599.ico -n $Filename mdf_channel_rename.py
